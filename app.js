34544f09b3e92aa1b50ccb74ca3fc60ac82308c1
amelierieser13-cyber <amelierieser13@gmail.com>
2025-11-27 12:30:26 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_URL = "DEINE_APPS_SCRIPT_URL_HIER"; // hier später Apps Script URL einfügen
+const API_URL = https://script.google.com/macros/s/AKfycbzlgJA4Cz79DgBKEZVnDsIanf82XS7wTnLPRQ2TBum3yKVU0PDj2dPWyxdE9UDPmdBj/exec; // hier später Apps Script URL einfügen
 
 async function loadRooms() {
   document.getElementById("rooms").innerText = "Räume werden geladen...";
