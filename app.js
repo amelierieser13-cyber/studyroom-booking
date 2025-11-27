4bbc6f73364a29a6e3ecb977ee63f9baff7123b2
amelierieser13-cyber <amelierieser13@gmail.com>
2025-11-27 12:25:42 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+const API_URL = "DEINE_APPS_SCRIPT_URL_HIER"; // hier später Apps Script URL einfügen
+
+async function loadRooms() {
+  document.getElementById("rooms").innerText = "Räume werden geladen...";
+}
+
+loadRooms();
